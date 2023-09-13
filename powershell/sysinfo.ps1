bee4d76ed8e1a9a4b4b6e160be0c013bcf64f4b3
workmaga <142840227+workmaga@users.noreply.github.com>
2023-09-13 15:08:25 -0400

    Add files via upload

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,12 @@
+﻿function getIP{ 
+
+    (get-netipaddress).ipv4address | Select-String "192*" 
+
+} 
+
+write-host(getIP)
+
+$IP = getIP
+
+write-host("This machine's IP is $IP")
+write-host("This machine's IP is {0}" -f $IP)
