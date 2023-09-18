5b9a4a7b36f29cb44b4d055dc6204de112f19a86
workmaga <142840227+workmaga@users.noreply.github.com>
2023-09-18 12:12:31 -0400

    Lab 3

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,43 @@
+﻿function getIP{ 
+
+    (get-netipaddress).ipv4address  | select-string "192.168.4"
+
+} 
+
+function getUser{
+
+    $env:USERNAME
+
+}
+
+function hostName{
+
+    $env:COMPUTERNAME
+}
+
+function psInfo{
+    
+    (get-host | select-object).Version.Major
+
+}
+
+function getDate{
+
+    Get-Date -Format "dddd, MMMM dd,yyyy"
+
+}
+
+$IP = getIP
+$HostName = hostName
+$VERSION = psInfo
+$USER = getUser
+$DATE = getDate
+
+
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HostName. PowerShell Version $VERSION. Today's Date is $DATE."
+Write-Host ($BODY)
+Write-Output ($BODY) | Out-File -FilePath .\OutFile.txt -Append
+
+
+
