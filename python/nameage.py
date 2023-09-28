93e01d20db64e54f549151be8e37c1696969e1c3
workmaga <workmaga@mail.uc.edu>
2023-09-28 10:03:48 -0400

    Python script commit

---
+++ python/nameage.py
@@ -0,0 +1,22 @@
+import time
+start_time = time.time()
+
+
+print("What is your name?")
+
+myName = input()
+
+print("Hello, " + myName + ". That is a good name. How old are you?")
+
+myAge = input() 
+programAge = int(time.time() - start_time)
+
+
+print("%s? That’s funny, I'm only %s seconds old." % (myAge, programAge)) 
+
+print("I wish I was %s years old" % (myAge * 2)) 
+
+time.sleep(3)
+
+print("I'm tired. I go sleep sleep now.")
+
---
+++ python/testpy.py
@@ -0,0 +1,5 @@
+myAge = input()
+
+print("%s? That’s funny, I'm only a few seconds old." % myAge)
+
+print("I wish I was %s years old" % (myAge * 2))
